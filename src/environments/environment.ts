94c0549ddc0ed4a5337d6ac90f111eead9b6a8e6
Johan Lopez <johanslopezc@gmail.com>
2024-05-27 09:15:58 -0500

    Despliegue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    devUrl: 'http://localhost:3000/api',
-    // devUrl: '',
-    proUrl: ''
-    // proUrl: 'https://pac-backend-tsuo.onrender.com/api'
+    // devUrl: 'http://localhost:3000/api',
+    devUrl: '',
+    // proUrl: ''
+    proUrl: 'https://pac-backend-tsuo.onrender.com/api'
 }
