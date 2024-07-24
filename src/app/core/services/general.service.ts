7d9c3c64839ac7eae512d6ea7a27aab1c3285d27
Johan Lopez <johanslopezc@gmail.com>
2024-07-24 08:26:33 -0500

    New psico

---
+++ src/app/core/services/general.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GeneralService {
+
+  private devUrl = environment.devUrl;
+  private proUrl = environment.proUrl;
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  createProduct(request: any):Observable<any> {
+    return this.httpClient.post<any>(this.devUrl + this.proUrl + '/general/createProduct', request)
+  }
+
+}
--- src/app/pages/undertake/undertake.component.ts
+++ src/app/pages/undertake/undertake.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { GeneralService } from 'src/app/core/services/general.service';
 
 @Component({
   selector: 'app-undertake',
   templateUrl: './undertake.component.html',
   styleUrls: ['./undertake.component.scss']
 })
-export class UndertakeComponent {
+export class UndertakeComponent implements OnInit {
+
+  dataForm!: FormGroup;
+  selectedFile: File | null = null;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private generalService: GeneralService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.dataForm = this.formBuilder.group({
+      title: [null, Validators.required],
+      subtitle: [null, Validators.required],
+      phone: [null, Validators.required],
+      address: [null, Validators.required],
+      products: [null, Validators.required],
+      facebook: [null, Validators.required],
+      mail: [null, Validators.required],
+    })
+  }
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0];
+    }
+  }
+
+  create() {
+    if (this.dataForm.invalid) {
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('title', this.dataForm.get('title')!.value);
+    formData.append('subtitle', this.dataForm.get('subtitle')!.value);
+    formData.append('phone', this.dataForm.get('phone')!.value);
+    formData.append('address', this.dataForm.get('address')!.value);
+    formData.append('products', this.dataForm.get('products')!.value);
+    formData.append('facebook', this.dataForm.get('facebook')!.value);
+    formData.append('mail', this.dataForm.get('mail')!.value);
+
+    if (this.selectedFile) {
+      formData.append('avatar', this.selectedFile, this.selectedFile.name);
+    }
+
+    this.generalService.createProduct(formData).subscribe({
+      next: () => {
+        alert('producto creado');
+        this.dataForm.reset();
+      }
+    })
+  }
+
+
 
 }
