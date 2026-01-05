19b564bb8f3a7f78af1a13aedc144f6d06a8be20
Johan Lopez <johanslopezc@gmail.com>
2026-01-05 15:23:47 -0500

    Env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: true,
-    devUrl: 'http://localhost:3000/api',
-    // devUrl: '',
-    proUrl: ''
-    // proUrl: 'https://pac-backend-tsuo.onrender.com/api'
+    // devUrl: 'http://localhost:3000/api',
+    devUrl: '',
+    // proUrl: ''
+    proUrl: 'https://pac-backend-tsuo.onrender.com/api'
     // proUrl: 'https://pack-back-production.up.railway.app'
 }
